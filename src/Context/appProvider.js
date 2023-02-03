5dc8fff1325c675272ea95749bc5dd23e033fd68
Saad Mehmood <saadmehmood131@gmail.com>
2023-02-03 22:51:00 +0500

    theme-switcher

---
+++ src/Context/appProvider.js
@@ -0,0 +1,22 @@
+import React, { createContext, useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles, theme } from "../Styles";
+
+export const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [themeMode, setThemeMode] = useState("lightTheme");
+
+  const value = { themeMode };
+  const costumTheme = theme[themeMode];
+
+  return (
+    <AppContext.Provider value={value}>
+      <ThemeProvider theme={costumTheme}>
+        <GlobalStyles />
+        {children}
+      </ThemeProvider>
+    </AppContext.Provider>
+  );
+};
+export default AppProvider;
